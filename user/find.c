c74bf1c3f0e4b6d99cba2e1f739479a24213ae00
liyuangang <liyuangang@sensetime.com>
2022-04-11 15:09:56 +0800

    util find.c fix bug

--- user/find.c
+++ user/find.c
@@ -6,45 +6,50 @@
 void
 find(char *path, char *filename)
 {
-  char buf[512], *p;
-  int fd;
-  struct dirent de;
-  struct stat st;
+    char buf[512], *p;
+    int fd;
+    struct dirent de;
+    struct stat st;
 
-  if((fd = open(path, 0)) < 0){
-    fprintf(2, "ls: cannot open %s\n", path);
-    return;
-  }
-
-  if(fstat(fd, &st) < 0){
-    fprintf(2, "ls: cannot stat %s\n", path);
-    close(fd);
-    return;
-  }
-
-  if(st.type!=T_DIR) {
-    fprintf(2, "usage: find <DIRECTORY> <filename>\n");
-    return;
-  }
-
-  if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
-      printf("ls: path too long\n");
-      return ;
+    if((fd = open(path, 0)) < 0){
+        fprintf(2, "ls: cannot open %s\n", path);
+        return;
     }
+
+    if(fstat(fd, &st) < 0){
+        fprintf(2, "ls: cannot stat %s\n", path);
+        close(fd);
+        return;
+    }
+
+    if(st.type!=T_DIR) {
+        fprintf(2, "usage: find <DIRECTORY> <filename>\n");
+        return;
+    }
+
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+        printf("ls: path too long\n");
+        return ;
+        }
     //把path复制到buf
     strcpy(buf, path);
     p = buf+strlen(buf);
     *p++ = '/';
     while(read(fd, &de, sizeof(de)) == sizeof(de)){
-      if(de.inum == 0)
-        continue;
-      memmove(p, de.name, DIRSIZ);
-      p[DIRSIZ] = 0;
-      if(stat(buf, &st) < 0){
-        printf("ls: cannot stat %s\n", buf);
-        continue;
-      }
-      printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+        if(de.inum == 0)
+            continue;
+        memmove(p, de.name, DIRSIZ);
+        p[DIRSIZ] = 0;
+        if(stat(buf, &st) < 0) {
+            printf("ls: cannot stat %s\n", buf);
+            continue;
+        }
+        if (st.type == T_DIR && strcmp(p,".") != 0 && strcmp(p,"..") != 0) {
+            find(buf,filename);
+        }else if (strcmp(filename, p) == 0) {
+            printf("%s\n", buf);
+        }
+        
     }
   close(fd);
 }
