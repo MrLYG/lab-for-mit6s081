6db57213fdec5e462d2c352dc3e991b53d1e42ec
liyuangang <liyuangang@sensetime.com>
2022-04-11 11:27:10 +0800

    add primes to Makefile

--- user/primes.c
+++ user/primes.c
@@ -10,12 +10,12 @@ void primes(int lp[2])
     int first;
     if(read(lp[RD],&first,sizeof(int)) == sizeof(int)) {
         printf("prime %d\n", first);
-
         //将不能被整除的数放入右管道
         int rp[2];
         pipe(rp); // 当前进程的右管道
         int data;
-        while (read(lp[RD],&first,sizeof(int)) == sizeof(int)) {
+        while (read(lp[RD],&data,sizeof(int)) == sizeof(int)) {
+            printf("call subprocess");
              // 将无法整除的数据传递入右管道
              if (data % first){
                  write(rp[WR],&data,sizeof(int));
@@ -25,15 +25,14 @@ void primes(int lp[2])
         close(rp[WR]);
         //创建子进程处理，递归处理
         if (fork() == 0) {
-            primes(rp);    // 递归的思想，但这将在一个新的进程中调用
+            // printf("call subprocess");
+            primes(rp);    
         } else {
             close(rp[RD]);
             wait(0);
         }
     }
-    
     exit(0);
-    
 }
 
 
